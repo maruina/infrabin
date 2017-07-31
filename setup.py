387e4068587190472089302913df66ab57b31336
Matteo Ruina <matteo.ruina@gmail.com>
2017-07-31 20:06:52 +0100

    Fix long description

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name = "infrabin",
@@ -17,7 +16,7 @@ setup(
     license = "MIT",
     keywords = "infrastructure bin",
     packages=['infrabin', 'tests'],
-    long_description=read('README.md'),
+    long_description=long_description,
     classifiers=[
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
